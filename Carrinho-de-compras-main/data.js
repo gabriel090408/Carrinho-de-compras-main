d331b8879a10f5384e1a4e5df753dd541e7d3e6a
gabriel090408 <gabi.sundl@gmil.com>
2026-08-14 01:09:04 -0300

    .

---
+++ Carrinho-de-compras-main/data.js
@@ -0,0 +1,38 @@
+ export const produtos = [
+    {
+        id: "1",
+        nome: "Geladeira",
+        preco: "2300,00",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqLzZvOnbHb8X9WEqt6andbgQNhMzqXsCZhWvuQmgqWw&s"
+    },
+    {
+        id: "2",
+        nome: "Sofá 3 lugares",
+        preco: "2459,00",
+        imagem: ""
+    },
+    {
+        id: "3",
+        nome: "Celular Xiaomi",
+        preco: "2189,90",
+        imagem: ""
+    },
+    {
+        id: "4",
+        nome: "Notebook",
+        preco: "4589,00",
+        imagem: ""
+    },
+    {
+        id: "5",
+        nome: "Caneca",
+        preco: "59,00",
+        imagem: ""
+    },
+    {
+        id: "6",
+        nome: "Playstation 4",
+        preco: "2229,89",
+        imagem: ""
+    }
+]
