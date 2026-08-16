df4b8be629035fb035f6f55537d26cec263a4bc4
gabriel090408 <gabi.sundl@gmil.com>
2026-08-16 14:25:55 -0300

    trabalho

--- Carrinho-de-compras-main/script.js
+++ Carrinho-de-compras-main/script.js
@@ -1,3 +1,28 @@
 import {produtos} from './data.js'
 
 console.log(produtos)
+
+const body = document.querySelector('body')
+
+function criarHeader () {  
+    const header = document.createElement('header')
+    const h1 = document.createElement ('h1')
+      
+    h1.innerText = 'KABUUM'
+    header.append(h1)
+     body.append (header)
+}
+
+criarHeader()
+
+function criarMain () {
+
+    const main = document.createElement('main')
+    const h2 = document.createElement('h2')
+    h2.innerText= "itens na loja"
+    const ul = document.createElement("ul")
+   
+     main.append(h2, ul)
+     body.append(main) 
+}
+criarMain()
